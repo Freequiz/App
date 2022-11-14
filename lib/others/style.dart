24f6f5426b217d76149a546205c86b6bd82c8108
Matthieu Cottet <matthieu.cottet@outlook.com>
2022-11-14 15:20:32 +0100

    Alpha 1.1.1

--- lib/others/style.dart
+++ lib/others/style.dart
@@ -4,6 +4,6 @@ Color color1 = const Color.fromARGB(255, 152, 141, 145);
 Color color2 = const Color.fromARGB(255, 230, 148, 150);
 Color color3 = const Color.fromARGB(255, 233, 209, 196);
 Color color4 = const Color.fromARGB(255, 209, 222, 230);
-Color color5 = const Color.fromARGB(255, 154, 137, 187);
+Color color5 = const Color.fromARGB(255, 154, 137, 188);
 Color backgroundGray = const Color.fromARGB(255, 50, 50, 50);
 Color textGray = const Color.fromARGB(255, 40, 40, 40);
