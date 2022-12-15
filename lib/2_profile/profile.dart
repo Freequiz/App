e055b719d96c90aac167e62e83beb2496456021a
Matthieu Cottet <matthieu.cottet@outlook.com>
2022-12-15 18:55:13 +0100

    Alpha 2.0.0

--- lib/2_profile/profile.dart
+++ lib/2_profile/profile.dart
@@ -1,26 +1,22 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Profile {
-  static String sessionToken = "";
-  static int date = 0;
+  static String accessToken = "";
   static bool loaded = false;
 
   Future<void> saveData() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString("sessionToken", sessionToken);
-    await prefs.setInt("date", date);
+    await prefs.setString("accessToken", accessToken);
   }
 
   Future<void> loadData() async {
     final prefs = await SharedPreferences.getInstance();
-    sessionToken = prefs.getString("sessionToken") ?? "";
-    date = prefs.getInt("date") ?? 0;
+    accessToken = prefs.getString("accessToken") ?? "";
     loaded = true;
   }
 
   Future<void> deleteData() async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.remove("sessionToken");
-    prefs.remove("date");
+    prefs.remove("accessToken");
   }
 }
