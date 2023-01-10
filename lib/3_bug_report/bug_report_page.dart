893048ea6bab9e25159008cebe76351c13d1d791
Matthieu Cottet <matthieu.cottet@outlook.com>
2023-01-10 21:36:51 +0100

    Lots of changes: Bug Report, AppBar, Error etc.

---
+++ lib/3_bug_report/bug_report_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:freequiz/3_bug_report/bug_reporter.dart';
+import 'package:freequiz/others/initial_loading.dart';
+import 'package:freequiz/others/style.dart';
+
+class BugReportPage extends StatefulWidget {
+  const BugReportPage({super.key});
+
+  @override
+  State<BugReportPage> createState() => _BugReportPageState();
+}
+
+class _BugReportPageState extends State<BugReportPage> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Center(
+      child: SizedBox(
+        height: height / 6,
+        width: width / 1.5,
+        child: TextButton(
+          style: TextButton.styleFrom(
+              backgroundColor: color1, foregroundColor: Colors.white),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return const BugReporter();
+                },
+              ),
+            );
+          },
+          child: Text(language["Report Bug"], style: TextStyle(fontSize: height / 40),),
+        ),
+      ),
+    );
+  }
+}
