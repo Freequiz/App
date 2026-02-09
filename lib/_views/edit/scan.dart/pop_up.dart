bd76c20dbd17438fa9d546e152d1613d0eb7274d
Matthieu Cottet <matthieu.cottet@outlook.com>
2026-02-09 17:22:48 +0100

    Improved UI for scanning

--- lib/_views/edit/scan.dart/pop_up.dart
+++ lib/_views/edit/scan.dart/pop_up.dart
@@ -2,10 +2,18 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:freequiz/_views/edit/scan.dart/scan.dart';
 import 'package:freequiz/controllers/edit/scan.dart';
 import 'package:freequiz/utilities/imports/base.dart';
+import 'package:freequiz/utilities/imports/utilities.dart';
 
-class ScanPopUp extends StatelessWidget {
+class ScanPopUp extends StatefulWidget {
   final Function refresh;
   const ScanPopUp({super.key, required this.refresh});
+
+  @override
+  State<ScanPopUp> createState() => _ScanPopUpState();
+}
+
+class _ScanPopUpState extends State<ScanPopUp> {
+  bool processing = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +22,15 @@ class ScanPopUp extends StatelessWidget {
         context.tr("scan quiz"),
         style: TextStyle(color: context.darkMode ? Colors.white : Colors.black),
       ),
-      content: SizedBox(),
+      content: Conditional(
+          condition: processing,
+          widget: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+            ],
+          ),
+          defaultWidget: const Text("scan quiz text").tr()),
       actions: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -25,15 +41,19 @@ class ScanPopUp extends StatelessWidget {
             ),
             TextButton(
               onPressed: () async {
+                setState(() {
+                  processing = true;
+                });
                 await Scan.main();
                 if (!context.mounted) return;
-                
+
                 Navigator.of(context).pop();
                 Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => ScanQuiz(refresh: refresh),
+                  builder: (BuildContext context) =>
+                      ScanQuiz(refresh: widget.refresh),
                 ));
               },
-              child: const Text("done").tr(),
+              child: Visibility(visible: !processing, child: const Text("okay").tr()),
             ),
           ],
         )
