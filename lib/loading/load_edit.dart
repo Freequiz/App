4cde99175a622d688a51f5f6ab6f833e9741c5eb
Matthieu Cottet <matthieu.cottet@outlook.com>
2026-02-24 11:59:29 +0100

    No connection message added to Edit Quit Overview

--- lib/loading/load_edit.dart
+++ lib/loading/load_edit.dart
@@ -1,9 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
 import 'package:freequiz/_views/edit/created_quizzes/created_quizzes.dart';
 import 'package:freequiz/_views/edit/created_quizzes/list_quizzes.dart';
+import 'package:freequiz/loading/no_connection/no_connection.dart';
+import 'package:freequiz/services/api/api.dart';
 import 'package:freequiz/services/api/users.dart';
-import 'package:freequiz/utilities/styles/font_style.dart';
+import 'package:freequiz/utilities/imports/utilities.dart';
 
 class LoadEdit extends StatelessWidget {
   final Key keyChild;
@@ -21,13 +22,19 @@ class LoadEdit extends StatelessWidget {
             ListQuizzes.more = data.data!['next_page'];
             return CreatedQuizzes(key: keyChild, refresh: refresh);
           }
+          if (data.data!['message'] == Api.noConnection || data.data!['message'] == Api.timeout) {
+            return const NoConnectionAlert(
+              backgroundColor: gray55,
+              showButton: false,
+            );
+          }
           return Text(
-            context.tr('${data.error ?? data.data?["token"] ?? "other error"}'),
+            context.tr('${data.error ?? data.data?["token"] ?? "other error title"}'),
             style: titleStyle(),
           );
         } else if (data.hasError) {
           return Text(
-            context.tr('${data.error ?? "other error"}'),
+            context.tr('${data.error ?? "other error title"}'),
             style: titleStyle(),
           );
         }
